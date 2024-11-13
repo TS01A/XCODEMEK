14b8996e7091d2e58b6b0a6f5ff9c286f754a537
Abdullah <ba266176@gmail.com>
2024-11-14 01:05:44 +0300

    login and register done

--- withyou/ViewController.swift
+++ withyou/ViewController.swift
@@ -45,12 +45,52 @@ class ViewController: UIViewController {
     }
     
     @IBAction func registerPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Register", message:"Register" , preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            let emailField = alert.textFields![0]
+            let passwordField = alert.textFields![1]
+            Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) {
+                user , error in
+                if error == nil {
+                    Auth.auth().signIn(withEmail: self.emailText.text!, password: self.passwordText.text!)
+                    Auth.auth().addStateDidChangeListener(){
+                        auth,user in
+                            if user != nil {
+                                self.performSegue(withIdentifier: self.loginToList, sender: nil)
+                            }
+                        
+                    }
+                }
+            }
+            
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter Your Email"
+            
+        }
+        alert.addTextField { textPasword in
+            textPasword.isSecureTextEntry = true
+            textPasword.placeholder = "Enter Your Password"
+            
+            
+        }
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert,animated: true,completion: nil)
     }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Auth.auth().addStateDidChangeListener(){
+            auth,user in
+            if user != nil {
+                self.performSegue(withIdentifier: self.loginToList, sender: nil)
+            }
+        }
     }
     
 }
