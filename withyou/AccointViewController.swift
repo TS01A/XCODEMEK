55adce8538db5da7b9c06e9e98840c76c6c852a4
Abdullah <ba266176@gmail.com>
2024-11-19 12:03:04 +0300

    myaccount

--- withyou/AccointViewController.swift
+++ withyou/AccointViewController.swift
@@ -9,26 +9,47 @@ import UIKit
 
 class AccointViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
     
+    @IBOutlet weak var copyInstructionLabel: UILabel!
+    @IBOutlet weak var emailDisplay: UITextView!
     
+   
     override func viewDidLoad() {
             super.viewDidLoad()
 
-            // إعداد صورة افتراضية
-            profileImage.image = UIImage(systemName: "person.crop.circle.badge.plus") // استخدام صورة من SF Symbols
-            
-            // تعيين نمط المحتوى
-            profileImage.contentMode = .scaleAspectFill // يملأ الصورة بشكل جيد
+            // إعداد عنوان "حسابك"
+            titleLabel.text = "حسابك"
+            titleLabel.textAlignment = .center // توسيط العنوان
+            titleLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold) // تعيين حجم الخط
 
-            
-            
+            // إعداد صورة افتراضية
+            profileImage.image = UIImage(systemName: "person.crop.circle.badge.plus")
+            profileImage.contentMode = .scaleAspectFill
+
+            // استرجاع البريد الإلكتروني
+            if let email = UserDefaults.standard.string(forKey: "userEmail") {
+                emailDisplay.text = email
+                emailDisplay.isEditable = false
+                emailDisplay.isSelectable = true
+            } else {
+                emailDisplay.text = "البريد الإلكتروني غير متوفر"
+                emailDisplay.isEditable = false
+                emailDisplay.isSelectable = false
+            }
+        emailDisplay.font = UIFont.systemFont(ofSize: 18)
+
             // إضافة إيماءة التفاعل مع الصورة
             let profileImageGesture = UITapGestureRecognizer(target: self, action: #selector(PickProfileImage))
             profileImage.addGestureRecognizer(profileImageGesture)
             profileImage.isUserInteractionEnabled = true
 
-            // قيود الصورة في يمين الشاشة من الأعلى
+            // إضافة إيماءة التفاعل مع البريد الإلكتروني
+            let emailTapGesture = UITapGestureRecognizer(target: self, action: #selector(copyEmail))
+            emailDisplay.addGestureRecognizer(emailTapGesture)
+
+            // إعداد القيود
             setupConstraints()
         }
 
@@ -37,6 +58,16 @@ class AccointViewController: UIViewController,UIImagePickerControllerDelegate,UI
             imagePicker.delegate = self
             imagePicker.sourceType = .photoLibrary
             present(imagePicker, animated: true, completion: nil)
+        }
+
+        @objc func copyEmail() {
+            // نسخ البريد الإلكتروني إلى الحافظة
+            UIPasteboard.general.string = emailDisplay.text
+            
+            // عرض تنبيه لتأكيد النسخ
+            let alert = UIAlertController(title: "تم النسخ", message: "البريد الإلكتروني قد تم نسخه إلى الحافظة.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "موافق", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
         }
 
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -51,13 +82,36 @@ class AccointViewController: UIViewController,UIImagePickerControllerDelegate,UI
         }
 
         private func setupConstraints() {
-            // إضافة قيود للصورة (لتكون في يمين الشاشة من الأعلى)
+            // إعداد قيود الصورة
             profileImage.translatesAutoresizingMaskIntoConstraints = false
+            emailDisplay.translatesAutoresizingMaskIntoConstraints = false
+            copyInstructionLabel.translatesAutoresizingMaskIntoConstraints = false
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
             NSLayoutConstraint.activate([
-                profileImage.widthAnchor.constraint(equalToConstant: 100),  // عرض الصورة
-                profileImage.heightAnchor.constraint(equalToConstant: 100), // ارتفاع الصورة
-                profileImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20), // المسافة من اليمين
-                profileImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 40) // المسافة من الأعلى
+                // قيود لعنوان "حسابك"
+                titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20), // المسافة من الجزء العلوي
+                titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor), // توسيط العنوان
+                
+                // قيود للصورة
+                profileImage.widthAnchor.constraint(equalToConstant: 100),
+                profileImage.heightAnchor.constraint(equalToConstant: 100),
+                profileImage.centerXAnchor.constraint(equalTo: view.centerXAnchor), // توسيط الصورة
+                profileImage.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20), // المسافة بين العنوان والصورة
+                
+                // قيود للبريد الإلكتروني
+                emailDisplay.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 20), // المسافة بين الصورة والبريد
+                emailDisplay.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20), // المسافة من اليسار
+                emailDisplay.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20), // المسافة من اليمين
+                emailDisplay.heightAnchor.constraint(equalToConstant: 40), // ارتفاع ثابت لـ UITextView
+                
+                // قيود لتعليمات النسخ
+                copyInstructionLabel.topAnchor.constraint(equalTo: emailDisplay.bottomAnchor, constant: 10), // المسافة بين البريد وتعليمات النسخ
+                copyInstructionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor) // توسيط التعليمات
             ])
+            
+            // إعداد تعليمات النسخ
+            copyInstructionLabel.text = "يمكنك نسخ البريد الإلكتروني بالنقر عليه"
+            copyInstructionLabel.textAlignment = .center
         }
     }
